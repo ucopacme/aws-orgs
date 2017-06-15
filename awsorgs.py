a7dd1231a16708d5ea8146d6197899c532cf20e2
ashely <agould@ucop.edu>
2017-06-14 19:37:59 -0700

    awsorgs: all globals gone.  got rid of several redundand defs.
    rework logic in manage_policy_attachements()

--- awsorgs.py
+++ awsorgs.py
@@ -85,38 +85,12 @@ def get_root_id(org_client):
     """
     Query deployed AWS Organization for its Root ID.
     """
-    try:
-        root_id = org_client.list_roots()['Roots'][0]['Id']
-        return root_id
-    except NoCredentialsError as e:
-        print sys.argv[0]
-        print e
-        print "at function:", inspect.getframeinfo(inspect.currentframe())[2]
-        print "in module:", __name__
-        raise SystemExit
-    except ClientError as e:
-        print e
-        print "at function:", inspect.getframeinfo(inspect.currentframe())[2]
-        print "in module:", __name__
-        raise SystemExit
-
-
-def parse_org_specification(args):
-    """
-    load yaml 'spec-file' into dictionary.
-    set global vars:
-        org_spec
-        default_policy
-        master_account
-    """
-    global org_spec, default_policy, master_account
-    org_spec = yaml.load(args.spec_file.read())
-    for policy in org_spec['policy_spec']:
-        if 'Default' in policy and policy['Default'] == True:
-            default_policy = policy['Name']
-    for account in org_spec['account_spec']:
-        if 'Master' in account and account['Master'] == True:
-            master_account = account['Name']
+    roots = org_client.list_roots()['Roots']
+    if len(roots) >1:
+        raise RuntimeError(
+            "org_client.list_roots returned multiple roots.  Go figure!"
+        )
+    return roots[0]['Id']
 
 
 def enable_policy_type_in_root(org_client, root_id):
@@ -170,72 +144,12 @@ def lookup(dlist, lkey, lvalue, rkey=None):
     return items[0]
 
 
-#def find_in_dictlist (dictlist, searchkey, searchvalue, returnkey):
-#    """
-#    Find a value in a list of dictionaries based on a known key:value.
-#    Return found value or 'None'.
-#
-#    args:
-#        dictlist:    data structure to search -  a list of type dictionary.
-#        seachkey:    name of key to use as search criteria
-#        seachvalue:  value to use as search criteria
-#        returnkey:   name of key indexing the value to return
-#    """
-#
-#    # make sure keys exist
-#    if not filter(lambda d: searchkey in d and returnkey in d, dictlist):
-#        #error: key not found
-#        return None
-#
-#    # check for duplicate search values
-#    values = map(lambda d: d[searchkey], dictlist)
-#    if len(values) != len(set(values)):
-#        # error: duplicate search values
-#        return None
-#
-#    # find the matching dictionary and return the indexed value
-#    result = filter(lambda d: d[searchkey] == searchvalue, dictlist)
-#    if len(result) == 1:
-#        return result[0][returnkey]
-#    else:
-#        return None
-
-
 
 
 
 #
 # Account functions
 #
-
-## Unused
-#def get_account_spec(ou_spec):
-#    if 'Account' in ou_spec and ou_spec['Account'] != None:
-#        return ou_spec['Account']
-#
-#Unused
-#def account_in_ou(account_id, ou_id):
-#    if get_parent_id(account_id) == ou_id:
-#        return True
-#    else:
-#        return False
-
-
-#def get_account_id_by_name(account_name):
-#    """
-#    search 'deployed_account' dictlist for 'account_name'. return the
-#    acccount Id or 'None'.
-#    """
-#    return find_in_dictlist(deployed_accounts, 'Name', account_name, 'Id')
-#    lookup(deployed_accounts, 'Name', account_name, 'Id')
-
-
-#def get_account_email_by_name(account_name):
-#    """
-#    search 'deployed_account' dictlist for 'account_name'. return the
-#    acccount Email or 'None'.
-#    """
-#    return find_in_dictlist(deployed_accounts, 'Name', account_name, 'Email')
 
 
 def scan_deployed_accounts(org_client):
@@ -317,39 +231,36 @@ def manage_accounts(org_client, args, deployed_accounts, deployed_ou, account_sp
     by designated OrganizationalUnits based on account specification
     ('account_spec').
     """
-    global change_counter
     for a_spec in account_spec:
-        if a_spec['Name'] != master_account:
-            account_name = a_spec['Name']
-            account_id = lookup(deployed_accounts, 'Name', account_name, 'Id')
+        account_id = lookup(deployed_accounts, 'Name', a_spec['Name'], 'Id')
 
-            if not account_id:
-                if args.create_accounts:
-                    change_counter += 1
-                    if not args.silent:
-                        print "creating account: %s" % (account_name)
-                    if not args.dry_run:
-                        account_state = create_account(org_client, a_spec)
-                else:
-                    if not args.silent:
-                        print "Warning: account %s not in Org." % (account_name)
-                        print "Use '--create-accounts' option to create new accounts."
-
+        if not account_id:
+            if args.create_accounts:
+                if not args.silent:
+                    print "creating account: %s" % (a_spec['Name'])
+                if not args.dry_run:
+                    account_state = create_account(org_client, a_spec)
             else:
-                # locate account in correct ou
-                parent_id = get_parent_id(org_client, account_id)
-                parent_ou_name = lookup(deployed_ou, 'Id', parent_id, 'Name')
-                if a_spec['OU'] != parent_ou_name:
-                    change_counter += 1
-                    if not args.silent:
-                        print "moving account %s from ou %s to ou %s" % (account_name, parent_ou_name, a_spec['OU'] )
-                    if not args.dry_run:
-                        ou_id = lookup(deployed_ou, 'Name', a_spec['OU'], 'Id')
-                        if ou_id:
-                            move_account(org_client, account_id, parent_id, ou_id)
-                        else:
-                            # handle execption: ou_id not found
-                            print 'error: ou_id not found'
+                if not args.silent:
+                    print "Warning: account %s not in Org." % (a_spec['Name'])
+                    print "Use '--create-accounts' option to create new accounts."
+
+        else:
+            # locate account in correct ou
+            parent_id = get_parent_id(org_client, account_id)
+            parent_ou_name = lookup(deployed_ou, 'Id', parent_id, 'Name')
+            if not 'OU' in a_spec or not a_spec['OU']:
+                a_spec['OU'] = 'root'
+            if a_spec['OU'] != parent_ou_name:
+                if not args.silent:
+                    print "moving account %s from ou %s to ou %s" % (a_spec['Name'], parent_ou_name, a_spec['OU'] )
+                if not args.dry_run:
+                    ou_id = lookup(deployed_ou, 'Name', a_spec['OU'], 'Id')
+                    if ou_id:
+                        move_account(org_client, account_id, parent_id, ou_id)
+                    else:
+                        # handle execption: ou_id not found
+                        print 'error: ou_id not found'
 
 
 
@@ -358,21 +269,6 @@ def manage_accounts(org_client, args, deployed_accounts, deployed_ou, account_sp
 # Policy functions
 #
 
-#def get_policy_id_by_name(policy_name):
-#    """
-#    search 'deployed_policies' dictlist for 'policy_name'. return the
-#    policy Id or 'None'.
-#    """
-#    return find_in_dictlist(deployed_policies, 'Name', policy_name, 'Id')
-
-
-#def get_policy_description(policy_id):
-#    """
-#    search 'deployed_policies' dictlist for 'policy_name'. return the
-#    policy Description or 'None'.
-#    """
-#    return find_in_dictlist(deployed_policies, 'Id', policy_id, 'Description')
-
 
 def get_policy_content(org_client, policy_id):
     """
@@ -380,7 +276,6 @@ def get_policy_content(org_client, policy_id):
     accociated with the Service Control Policy referenced by 'policy_id'.
     """
     return org_client.describe_policy(PolicyId=policy_id)['Policy']['Content']
-    # handle exception
 
 
 def list_policies_in_ou (org_client, ou_id):
@@ -393,18 +288,6 @@ def list_policies_in_ou (org_client, ou_id):
         Filter='SERVICE_CONTROL_POLICY',
     )['Policies']
     return sorted(map(lambda ou: ou['Name'], policies_in_ou))
-
-
-def get_policy_spec_for_ou(ou_spec):
-    """
-    Search 'ou_spec' dict for a list of policy names specified for attachment
-    to this OrganizationalUnit.  Prepend the 'default_policy' to this list
-    and return list.
-    """
-    if 'Policy' in ou_spec and ou_spec['Policy'] != None:
-        return [default_policy] + ou_spec['Policy']
-    else:
-        return [default_policy]
 
 
 def specify_policy_content(p_spec):
@@ -448,41 +331,6 @@ def delete_policy(org_client, policy_id):
     org_client.delete_policy(PolicyId=policy_id)
 
 
-def policy_attached(org_client, policy_id, ou_id,):
-    """
-    Test if a deployed Service Control Policy ('policy_id') is attached to a
-    given OrganizationalUnit ('ou_id').  Returns a boolean.
-    """
-    policy_targets = org_client.list_targets_for_policy (
-        PolicyId=policy_id
-    )['Targets']
-    if ou_id in map(lambda ou: ou['TargetId'], policy_targets):
-        return True
-    return False
-
-
-def attach_policy (org_client, policy_id, ou_id,):
-    """
-    Attach a deployed Service Control Policy ('policy_id') to a given
-    OrganizationalUnit ('ou_id').
-    """
-    org_client.attach_policy (
-        PolicyId=policy_id,
-        TargetId=ou_id
-    )
-
-
-def detach_policy (org_client, policy_id, ou_id,):
-    """
-    Detach a deployed Service Control Policy ('policy_id') from a given
-    OrganizationalUnit ('ou_id').
-    """
-    org_client.detach_policy (
-        PolicyId=policy_id,
-        TargetId=ou_id
-    )
-
-
 def display_provissioned_policies(org_client, deployed_policies):
     """
     Print report of currently deployed Service Control Policies in
@@ -505,34 +353,29 @@ def manage_policies(org_client, args, deployed_policies, policy_spec):
     Manage Service Control Policies in the AWS Organization.  Make updates
     according to the policy specification ('policy_spec').
     """
-    global change_counter
     for p_spec in policy_spec:
-        if p_spec['Name'] != default_policy:
-            policy_name = p_spec['Name']
-            policy_id = lookup(deployed_policies, 'Name', policy_name, 'Id')
+        policy_name = p_spec['Name']
+        policy_id = lookup(deployed_policies, 'Name', policy_name, 'Id')
 
-            if policy_id and ensure_absent(p_spec):
-                change_counter += 1
+        if policy_id and ensure_absent(p_spec):
+            if not args.silent:
+                print "deleting policy: %s" % (policy_name)
+            if not args.dry_run:
+                delete_policy(org_client, policy_id)
+
+        else:
+            if not policy_id:
                 if not args.silent:
-                    print "deleting policy: %s" % (policy_name)
+                    print "creating policy: %s" % (policy_name)
                 if not args.dry_run:
-                    delete_policy(org_client, policy_id)
+                    create_policy(org_client, p_spec)
 
             else:
-                if not policy_id:
-                    change_counter += 1
+                if p_spec['Description'] != lookup(deployed_policies, 'Id', policy_id, 'Description') or specify_policy_content(p_spec) != get_policy_content(org_client, policy_id):
                     if not args.silent:
-                        print "creating policy: %s" % (policy_name)
+                        print "updating policy: %s" % (policy_name)
                     if not args.dry_run:
-                        create_policy(org_client, p_spec)
-
-                else:
-                    if p_spec['Description'] != lookup(deployed_policies, 'Id', policy_id, 'Description') or specify_policy_content(p_spec) != get_policy_content(org_client, policy_id):
-                        change_counter += 1
-                        if not args.silent:
-                            print "updating policy: %s" % (policy_name)
-                        if not args.dry_run:
-                            update_policy(org_client, p_spec, policy_id)
+                        update_policy(org_client, p_spec, policy_id)
 
 
 
@@ -576,7 +419,7 @@ def children_in_ou_spec(ou_spec):
     """
     Check if if 'ou_spec' has any child OU.  Returns boolean.
     """
-    if 'OU' in ou_spec and ou_spec['OU'] != None and len(ou_spec['OU']) != 0:
+    if 'OU' in ou_spec and isinstance(ou_spec['OU'], list):
         return True
     return False
 
@@ -643,67 +486,62 @@ def manage_policy_attachments(org_client, args, deployed_policies, ou_spec, ou_i
     Attach or detach specified Service Control Policy ('ou_spec') to a
     deployed OrganizatinalUnit ('ou_id)'.
     """
-    global change_counter
-    # attach specified policies
-    p_spec = get_policy_spec_for_ou(ou_spec)
-    for policy_name in p_spec:
-        policy_id = lookup(deployed_policies, 'Name', policy_name, 'Id')
+    
+    attached_policy_list = list_policies_in_ou(org_client, ou_id)
+    if 'Policy' in ou_spec and isinstance(ou_spec['Policy'],list):
+        spec_policy_list = ou_spec['Policy']
+    else:
+        spec_policy_list = []
+    policies_to_attach = [p for p in spec_policy_list
+            if p not in attached_policy_list]
+    policies_to_detach = [p for p in attached_policy_list
+            if p not in spec_policy_list
+            and p != 'FullAWSAccess']
 
-        if not policy_attached(org_client, policy_id, ou_id) and not ensure_absent(ou_spec):
-            change_counter += 1
+    for policy_name in policies_to_attach:
+        if not ensure_absent(ou_spec):
             if not args.silent:
                 print "attaching policy %s to OU %s" % (policy_name, ou_spec['Name'])
             if not args.dry_run:
-                attach_policy(org_client, policy_id, ou_id)
+                org_client.attach_policy(
+                        PolicyId=lookup(deployed_policies, 'Name', policy_name, 'Id'),
+                        TargetId=ou_id)
 
-    # detach unspecified policies
-    policy_list = list_policies_in_ou(org_client, ou_id)
-    for policy_name in policy_list:
-        if policy_name not in p_spec and not ensure_absent(ou_spec):
-            change_counter += 1
-            policy_id = lookup(deployed_policies, 'Name', policy_name, 'Id')
-            if not args.silent:
-                print "detaching policy %s from OU %s" % (policy_name, ou_spec['Name'])
-            if not args.dry_run:
-                detach_policy(org_client, policy_id, ou_id)
-
+    for policy_name in policies_to_detach:
+        if not args.silent:
+            print "detaching policy %s from OU %s" % (policy_name, ou_spec['Name'])
+        if not args.dry_run:
+            org_client.detach_policy(
+                    PolicyId=lookup(deployed_policies, 'Name', policy_name, 'Id'),
+                    TargetId=ou_id)
 
 
-def manage_ou (org_client, args, deployed_ou, deployed_policies, ou_spec_list, parent_name):
+
+def manage_ou (org_client, args, deployed_ou, deployed_policies,
+        ou_spec_list, parent_name):
     """
     Recursive function to manage OrganizationalUnits in the AWS Organization.
     """
-    global change_counter
     for ou_spec in ou_spec_list:
 
         # ou exists
         if lookup(deployed_ou, 'Name', ou_spec['Name']):
             if children_in_ou_spec(ou_spec):
                 # recurse
-                manage_ou(
-                        org_client,
-                        args,
-                        deployed_ou,
-                        deployed_policies,
-                        ou_spec['OU'],
-                        ou_spec['Name'])
+                manage_ou( org_client, args, deployed_ou, deployed_policies,
+                        ou_spec['OU'], ou_spec['Name'])
             if ensure_absent(ou_spec):
-                change_counter += 1
                 if not args.silent:
                     print 'deleting OU', ou_spec['Name']
                 if not args.dry_run:
                     delete_ou(org_client, deployed_ou, ou_spec['Name'])
             else:
-                manage_policy_attachments(
-                        org_client,
-                        args,
-                        deployed_policies,
+                manage_policy_attachments( org_client, args, deployed_policies,
                         ou_spec,
                         lookup(deployed_ou, 'Name', ou_spec['Name'], 'Id'))
 
         # ou does not exist
         elif not ensure_absent(ou_spec):
-            change_counter += 1
             if not args.silent:
                 print "creating new ou %s under parent %s" % (ou_spec['Name'], parent_name)
             if not args.dry_run:
@@ -711,8 +549,9 @@ def manage_ou (org_client, args, deployed_ou, deployed_policies, ou_spec_list, p
                         lookup(deployed_ou, 'Name', parent_name, 'Id'))
                 manage_policy_attachments( org_client, args, deployed_policies,
                         ou_spec, new_ou['Id'])
-                if children_in_ou_spec(ou_spec) and isinstance(new_ou, dict) \
-                        and 'Id' in new_ou:
+                if (children_in_ou_spec(ou_spec) and 
+                        isinstance(new_ou, dict) and
+                        'Id' in new_ou):
                     # recurse
                     manage_ou( org_client, args, deployed_ou, deployed_policies,
                             ou_spec['OU'], new_ou['Name'])
@@ -734,10 +573,6 @@ def main():
     # determine the Organization Root ID
     root_id = get_root_id(org_client)
 
-    # initialize change counter
-    global change_counter
-    change_counter = 0
-
     # scan deployed resources in Organization
     deployed_policies = org_client.list_policies(
         Filter='SERVICE_CONTROL_POLICY'
@@ -758,34 +593,30 @@ def main():
 
     # process organization spec-file
     else:
-        # read org-spec yaml file into dictionary - initializes globals:
-        #   org_spec
-        #   default_policy
-        #   master_account
-        parse_org_specification(args)
+        # read org-spec yaml file into dictionary
+        org_spec = yaml.load(args.spec_file.read())
 
         if args.dry_run: print "\nThis is a dry run!"
 
         if args.build_policy:
             manage_policies(org_client, args, deployed_policies,
                     org_spec['policy_spec'])
-        if args.build_account:
-            manage_accounts(org_client, args, deployed_accounts,
-                    deployed_ou, org_spec['account_spec'])
         if args.build_ou:
             enable_policy_type_in_root(org_client, root_id)
             manage_ou( org_client, args, deployed_ou, deployed_policies,
                     org_spec['organizational_unit_spec'], 'root')
+        if args.build_account:
+            manage_accounts(org_client, args, deployed_accounts,
+                    deployed_ou, org_spec['account_spec'])
 
         # run follow-up report
         if not args.silent and not args.no_report:
-            if change_counter > 0:
-                # rescan deployed resources
-                deployed_policies = org_client.list_policies(
-                    Filter='SERVICE_CONTROL_POLICY'
-                )['Policies']
-                deployed_accounts = scan_deployed_accounts(org_client)
-                deployed_ou = scan_deployed_ou(org_client, root_id)
+            # rescan deployed resources
+            deployed_policies = org_client.list_policies(
+                Filter='SERVICE_CONTROL_POLICY'
+            )['Policies']
+            deployed_accounts = scan_deployed_accounts(org_client)
+            deployed_ou = scan_deployed_ou(org_client, root_id)
             if args.build_policy:
                 display_provissioned_policies(org_client, deployed_policies)
             if args.build_account:
