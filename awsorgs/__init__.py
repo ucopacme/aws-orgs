05649ceb174e1b7d40313ced14bf48322857a595
ashely <agould@ucop.edu>
2017-07-21 16:40:58 -0700

    clean up __init__.py a bit

--- awsorgs/__init__.py
+++ awsorgs/__init__.py
@@ -3,14 +3,6 @@ import os
 import boto3
 
 
-def ensure_absent(spec):
-    """
-    test if an 'Ensure' key is set to absent in dictionary 'spec'
-    """
-    if 'Ensure' in spec and spec['Ensure'] == 'absent': return True
-    return False
-
-
 def lookup(dlist, lkey, lvalue, rkey=None):
     """
     Use a known key:value pair to lookup a dictionary in a list of
@@ -39,7 +31,18 @@ def lookup(dlist, lkey, lvalue, rkey=None):
     return items[0]
 
 
+def ensure_absent(spec):
+    """
+    test if an 'Ensure' key is set to absent in dictionary 'spec'
+    """
+    if 'Ensure' in spec and spec['Ensure'] == 'absent': return True
+    return False
+
+
 def logger(log, message):
+    """
+    Collect log messages into a list.
+    """
     if message:
         log.append(message)
     return
@@ -51,8 +54,7 @@ def get_root_id(org_client):
     """
     roots = org_client.list_roots()['Roots']
     if len(roots) >1:
-        raise RuntimeError(
-          "org_client.list_roots returned multiple roots.  Go figure!")
+        raise RuntimeError("org_client.list_roots returned multiple roots.")
     return roots[0]['Id']
 
 
@@ -70,8 +72,8 @@ def validate_master_id(org_client, spec):
     return
 
 
-def get_client_for_assumed_role(service_name, account_id,
-        role_name, region_name=None):
+def get_client_for_assumed_role(service_name, account_id, role_name,
+        region_name=None):
     """
     Get temporary sts assume_role credentials for account.
     Return boto3 client object with assumed role credentials attached.
@@ -79,15 +81,12 @@ def get_client_for_assumed_role(service_name, account_id,
     role_arn = 'arn:aws:iam::' + account_id + ':role/' + role_name
     role_session_name = account_id + '-' + role_name
     sts_client = boto3.client('sts')
-    credentials = sts_client.assume_role(
-            RoleArn=role_arn,
-            RoleSessionName=role_session_name
-            )['Credentials']
-    return boto3.client(
-            service_name,
-            aws_access_key_id = credentials['AccessKeyId'],
-            aws_secret_access_key = credentials['SecretAccessKey'],
-            aws_session_token = credentials['SessionToken'],
+    credentials = sts_client.assume_role( RoleArn=role_arn,
+            RoleSessionName=role_session_name)['Credentials']
+    return boto3.client(service_name,
+            aws_access_key_id=credentials['AccessKeyId'],
+            aws_secret_access_key=credentials['SecretAccessKey'],
+            aws_session_token=credentials['SessionToken'],
             region_name=region_name)
 
 
